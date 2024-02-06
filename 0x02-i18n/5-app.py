6d223dcd415afd61959bd1d20d5f37742531241e
niyioo <niyiolagbegi@gmail.com>
2024-02-06 17:20:32 +0100

    task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel setup, locale selector, parametrized templates,
+and support for user login emulation
+"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for setting up Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+# Mock user table
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id):
+    """Return user dictionary based on user ID"""
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """Set user in global flask.g object"""
+    user_id = request.args.get('login_as')
+    g.user = get_user(int(user_id)) if user_id else None
+
+
+@app.route('/')
+def index():
+    """Render index.html template"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
