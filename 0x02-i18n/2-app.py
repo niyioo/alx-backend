b5069e55f56c24953654df037d7ce31c2378a7b8
niyioo <niyiolagbegi@gmail.com>
2024-02-06 16:49:34 +0100

    b

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,16 +7,14 @@ from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
-app = Flask(__name__)
-
-
-class Config:
+class Config(object):
     """Config class for setting up Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
@@ -33,5 +31,5 @@ def index() -> str:
     return render_template('2-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
