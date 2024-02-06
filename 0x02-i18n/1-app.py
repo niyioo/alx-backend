64ad0400aff81747c63501b470f278e3028d530e
niyioo <niyiolagbegi@gmail.com>
2024-02-06 15:36:42 +0100

    nnn

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel setup
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for setting up Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """Render index.html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel setup and locale selector
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+import os
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for setting up Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Determine the best-matching locale for the client"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """Render index.html template"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
