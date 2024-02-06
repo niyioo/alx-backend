5e36f8c1a61111d702cba51175a5092e30c2816d
niyioo <niyiolagbegi@gmail.com>
2024-02-06 17:49:21 +0100

    n

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -31,27 +31,34 @@ users = {
 }
 
 
-def get_user(user_id):
+def get_user() -> Union[Dict, None]:
     """Return user dictionary based on user ID"""
-    return users.get(user_id)
+    id = request.args.get('login_as', None)
+    if id and int(id) in users.keys():
+        return users.get(int(id))
+    return None
 
 
 @app.before_request
 def before_request():
     """Set user in global flask.g object"""
-    user_id = request.args.get('login_as')
-    g.user = get_user(int(user_id)) if user_id else None
+    user = get_user()
+    g.user = user
 
 
 @babel.localeselector
 def get_locale():
     """Determine the best-matching locale for the client"""
-    if 'locale' in request.args:
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
             return locale
-    if g.user and g.user['locale'] in app.config['LANGUAGES']:
-        return g.user['locale']
+    locale = request.headers.get('locale', None)
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
