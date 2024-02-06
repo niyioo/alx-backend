c004c259c7e9cdfd2bec59580d9bda2eb067a80e
niyioo <niyiolagbegi@gmail.com>
2024-02-06 16:40:12 +0100

    n

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,44 +1,37 @@
 #!/usr/bin/env python3
 """
-Flask app
+Basic Flask app with Babel setup and locale selector
 """
-from flask import (
-    Flask,
-    render_template,
-    request
-)
-from flask_babel import Babel
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
-class Config(object):
-    """
-    Configuration for Babel
-    """
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for setting up Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
+    """Determine the best-matching locale for the client"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """
-    Handles / route
-    """
+    """Render index.html template"""
     return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
