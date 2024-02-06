023ddbd55eb9ecb136278039bdffb02f47e52a9e
niyioo <niyiolagbegi@gmail.com>
2024-02-06 16:23:50 +0100

    fx

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,8 +27,8 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
+@app.route('/', strict_slashes=False)
+def index() -> str:
     """Render index.html template"""
     return render_template('2-index.html')
 
